c00f08216cb9d82ef6c8438d3142486e3d58a00d
richard <richard.anne@epitech.eu>
2022-12-14 16:47:03 +0100

    [FIX] fix bulk load

--- scripts/bulk-load-minio.sh
+++ scripts/bulk-load-minio.sh
@@ -7,8 +7,8 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-docker run --entrypoint "/bin/bash" -v "$1:/data" --rm minio/mc "-c" \
-    "/usr/bin/mc alias set thu-ddbs http://host.docker.internal:9000 admin password ; \
+docker run --entrypoint "/bin/bash" -v "$1:/data" --network="host" --rm minio/mc "-c" \
+    "/usr/bin/mc alias set thu-ddbs http://127.0.0.1:9000 admin password ; \
      /usr/bin/mc mb thu-ddbs/thu-ddbs ; \
      /usr/bin/mc mirror /data thu-ddbs/thu-ddbs ; \
      /usr/bin/mc anonymous set public thu-ddbs/thu-ddbs"
