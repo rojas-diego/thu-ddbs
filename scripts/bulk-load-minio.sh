0ed06a595f9af43125e59e8cfe9597df868fddbe
Diego ROJAS <rojasdiegopro@gmail.com>
2022-12-14 22:09:07 +0800

    fix: minio import and add redis
    Signed-off-by: Diego ROJAS <rojasdiegopro@gmail.com>

--- scripts/bulk-load-minio.sh
+++ scripts/bulk-load-minio.sh
@@ -7,4 +7,8 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-docker run --entrypoint "/bin/bash" -v "$1:/data" --rm minio/mc "-c" "/usr/bin/mc alias set thuddbs http://host.docker.internal:9000 admin password && /usr/bin/mc mirror /data thuddbs/thuddbs"
+docker run --entrypoint "/bin/bash" -v "$1:/data" --rm minio/mc "-c" \
+    "/usr/bin/mc alias set thu-ddbs http://host.docker.internal:9000 admin password ; \
+     /usr/bin/mc mb thu-ddbs/thu-ddbs ; \
+     /usr/bin/mc mirror /data thu-ddbs/thu-ddbs ; \
+     /usr/bin/mc anonymous set public thu-ddbs/thu-ddbs"
