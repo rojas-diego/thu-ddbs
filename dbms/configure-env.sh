fe68c754cc94bbb19a5becbf9e912d541fdb5337
Diego ROJAS <rojasdiegopro@gmail.com>
2022-12-20 13:33:57 +0800

    Add configure env script and update all.sh
    Signed-off-by: Diego ROJAS <rojasdiegopro@gmail.com>

---
+++ dbms/configure-env.sh
@@ -0,0 +1,3 @@
+export DBMS_MONGO_URL=mongodb://localhost:27017
+export DBMS_REDIS_URL=http://localhost:6379
+export DBMS_OBJECT_STORAGE_PREFIX=https://thu-ddbs-asia.s3.ap-northeast-2.amazonaws.com/
--- scripts/all.sh
+++ scripts/all.sh
@@ -2,3 +2,5 @@ docker compose up -d || docker-compose up -d
 python3 scripts/gen_data_10GB.py
 ./scripts/setup-mongo-cluster.sh
 python3 ./scripts/bulk_load_mongo.py
+
+which mc && ./scripts/bulk-load-minio-local.sh ./data/article-data
